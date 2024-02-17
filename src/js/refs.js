38224b0a9b4e40f70539c01ef8dae6b4b7388750
Jara-Shamot <141449562+Jara-Shamot@users.noreply.github.com>
2024-02-17 11:14:16 +0000

    Deploying to gh-pages from @ Jara-Shamot/goit-js-hw-12@1220581de626ac721866288055efb459351854a9 🚀

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-  searchFormRef: document.querySelector('.search-form'),
-  galleryRef: document.querySelector('.gallery'),
-  spanLoaderRef: document.querySelector('.span-loader'),
-  loadMoreBtnRef: document.getElementById('btn'),
-};
--- src/js/spanLoader.js
+++
@@ -1,9 +0,0 @@
-import { refs } from './refs';
-export const showLoader = () => {
-  refs.spanLoaderRef.style.display = 'block';
-  refs.spanLoaderRef.style.visibility = 'visible';
-};
-export const hiddenLoader = () => {
-  refs.spanLoaderRef.style.display = 'none';
-  refs.spanLoaderRef.style.visibility = 'hidden';
-};
